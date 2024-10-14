4568449057add0cb2216e9a77f7f8402a3fba702
NeerajK007 <neeraj.kumar32@wipro.com>
2024-10-14 19:18:51 +0530

    test case setup done, 1 sample test case is working now

--- src/modules/discovery/discovery.controller.spec.ts
+++ src/modules/discovery/discovery.controller.spec.ts
@@ -1,15 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DiscoveryController } from './discovery.controller';
 
+import { DiscoveryService } from "./providers/discovery.service";
+
 describe('DiscoveryController', () => {
   let controller: DiscoveryController;
+  let discoveryService: DiscoveryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DiscoveryController],
+      providers: [
+        {
+          provide: DiscoveryService,
+          useValue: {
+            // Mock the actual method(s) used by the controller
+            getDiscoveryData: jest.fn().mockResolvedValue({ data: 'mocked response' }),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<DiscoveryController>(DiscoveryController);
+    discoveryService = module.get<DiscoveryService>(DiscoveryService);
   });
 
   it('should be defined', () => {
